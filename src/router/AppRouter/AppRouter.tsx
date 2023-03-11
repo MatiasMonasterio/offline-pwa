b44e4459da3d2f55cd830aa3f145d4120547c8cd
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-03-11 16:03:56 -0300

    feat: implements dashboard router with publics/privates route

--- src/router/AppRouter/AppRouter.tsx
+++ src/router/AppRouter/AppRouter.tsx
@@ -1,13 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { PublicRoutes, PrivateRoutes } from "hocs";
+
+import { AuthenticateProvider } from "@/authenticate/contexts";
+
 import { AuthenticateRouter } from "@/authenticate/router";
+import { DashboardRouter } from "@/dashboard/router";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/authenticate/*" element={<AuthenticateRouter />} />
-      </Routes>
+      <AuthenticateProvider>
+        <Routes>
+          <Route
+            path="/authenticate/*"
+            element={
+              <PublicRoutes>
+                <AuthenticateRouter />
+              </PublicRoutes>
+            }
+          />
+
+          <Route
+            path="/*"
+            element={
+              <PrivateRoutes>
+                <DashboardRouter />
+              </PrivateRoutes>
+            }
+          />
+        </Routes>
+      </AuthenticateProvider>
     </BrowserRouter>
   );
 }
