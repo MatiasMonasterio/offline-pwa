a02c9b7de07da9613142e2c8260526b684cef319
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-03-11 15:53:52 -0300

    feat: implements title to go back row action

--- src/components/GoBackRow/GoBackRow.tsx
+++ src/components/GoBackRow/GoBackRow.tsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Box } from "@chakra-ui/react";
+import { Button, Heading, Box } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 
-export default function GoBackRow() {
+interface Props {
+  title?: string;
+}
+
+export default function GoBackRow({ title }: Props) {
   const navigate = useNavigate();
 
   const handleGoBack = () => {
@@ -9,10 +14,21 @@ export default function GoBackRow() {
   };
 
   return (
-    <Box mt={2} mb={4}>
-      <Button size="sm" onClick={handleGoBack}>
-        Back
+    <Box display="flex" alignItems="center" mb={2} position="relative">
+      <Button
+        size="xs"
+        onClick={handleGoBack}
+        variant="link"
+        fontSize="2xl"
+        position="absolute"
+        left={0}
+      >
+        <ChevronLeftIcon />
       </Button>
+
+      <Heading size="md" mx="auto">
+        {title}
+      </Heading>
     </Box>
   );
 }
