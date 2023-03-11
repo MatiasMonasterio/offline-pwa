47ab2acf1c73498ead00e3d6505c34f6d6b3dd9a
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-03-11 15:58:06 -0300

    feat: add storages mock

---
+++ src/modules/dashboard/__mocks__/storages.mock.ts
@@ -0,0 +1,59 @@
+import type { Storage } from "@/dashboard/types";
+
+export const storagesMock: Storage[] = [
+  {
+    id: 1,
+    name: "Dubble - Hamburguesas",
+    rate: 4.5,
+  },
+  {
+    id: 2,
+    name: "Otaku Sushi",
+    rate: 4.4,
+  },
+  {
+    id: 3,
+    name: "Dubble - Hamburguesas",
+    rate: 4.4,
+  },
+  {
+    id: 4,
+    name: "Otaku Sushi",
+    rate: 4.4,
+  },
+  {
+    id: 5,
+    name: "Dubble - Hamburguesas",
+    rate: 4.4,
+  },
+  {
+    id: 6,
+    name: "Otaku Sushi",
+    rate: 4.4,
+  },
+  {
+    id: 7,
+    name: "Dubble - Hamburguesas",
+    rate: 4.4,
+  },
+  {
+    id: 8,
+    name: "Otaku Sushi",
+    rate: 4.4,
+  },
+  {
+    id: 9,
+    name: "Dubble - Hamburguesas",
+    rate: 4.4,
+  },
+  {
+    id: 10,
+    name: "Otaku Sushi",
+    rate: 4.4,
+  },
+  {
+    id: 11,
+    name: "Dubble - Hamburguesas",
+    rate: 4.4,
+  },
+];
