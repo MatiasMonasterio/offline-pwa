6f5c97f30b985f008cbbe3822fcfa86052cee926
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-03-11 20:15:50 -0300

    feat: add profile page to dashboard router

--- src/modules/dashboard/router/DashboardRouter/DashboardRouter.tsx
+++ src/modules/dashboard/router/DashboardRouter/DashboardRouter.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { DashboardLayout } from "@/dashboard/layouts";
 import HomePage from "@/dashboard/pages/Home";
+import ProfilePage from "@/dashboard/pages/Profile";
 
 export default function DashboardRouter() {
   return (
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Route>
     </Routes>
   );
